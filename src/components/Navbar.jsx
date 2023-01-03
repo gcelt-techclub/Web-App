6eb12b7f1500ca4fc7096486d924967824359322
PRIME_SAPTARSHI <chatterjeeu7@gmail.com>
2023-01-03 21:00:54 +0530

    added new changes

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-dom'
+import * as FaIcons from "react-icons/fa"
+
+function Navbar() {
+  return (
+    <>
+        <div className="navbar">
+        <Link to="#" className='menu-bars'>
+
+        </Link>
+        </div>
+    </>
+  )
+}
+
+export default Navbar

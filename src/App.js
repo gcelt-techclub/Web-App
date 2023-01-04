90d2d3374621b7acfecc9d8364e7f52bd7bea59f
CrazyCoder122 <prodoshcmitterfeluda@gmail.com>
2023-01-04 12:53:01 +0530

    Added the Conditional Routing: /admin

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Sidebar from './components/Sidebar';
-import Dashboard from './pages/Dashboard.jsx';
-import Teams from './pages/Teams.jsx';
-import Sports from './pages/Sports.jsx';
-import Tournament from './pages/Tournament.jsx';
-import Winners from './pages/Winners.jsx';
-import FeeRecords from './pages/FeeRecords.jsx';
+import React from "react";
+import "./App.css";
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+} from "react-router-dom";
+import Sidebar from "./components/Sidebar";
+import Dashboard from "./pages/Dashboard.jsx";
+import Teams from "./pages/Teams.jsx";
+import Sports from "./pages/Sports.jsx";
+import Tournament from "./pages/Tournament.jsx";
+import Winners from "./pages/Winners.jsx";
+import FeeRecords from "./pages/FeeRecords.jsx";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Sidebar>
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/teams" element={<Teams />} />
-          <Route path="/sports" element={<Sports />} />
-          <Route path="/tournament" element={<Tournament />} />
-          <Route path="/winners" element={<Winners />} />
-          <Route path="/feeRecords" element={<FeeRecords />} />
-        </Routes>
-      </Sidebar>
+      <Routes>
+        <Route path="/admin" element={<Sidebar />}>
+          <Route index element={<Dashboard />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="teams" element={<Teams />} />
+          <Route path="sports" element={<Sports />} />
+          <Route path="tournament" element={<Tournament />} />
+          <Route path="winners" element={<Winners />} />
+          <Route path="feeRecords" element={<FeeRecords />} />
+        </Route>
+
+        <Route path="/user" element={<h1>User</h1> }>
+
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 };
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,75 +1,86 @@
-import React, { useState } from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { useState } from "react";
+import { NavLink, Outlet, useOutletContext } from "react-router-dom";
 import "./Sidebar.css";
 
 //import icons
-import DashboardOutlinedIcon from '@mui/icons-material/DashboardOutlined';
-import Groups3RoundedIcon from '@mui/icons-material/Groups3Rounded';
+import DashboardOutlinedIcon from "@mui/icons-material/DashboardOutlined";
+import Groups3RoundedIcon from "@mui/icons-material/Groups3Rounded";
 import { GiPodium } from "react-icons/gi";
 import { GiTrophyCup } from "react-icons/gi";
-import SportsBaseballIcon from '@mui/icons-material/SportsBaseball';
-import RequestQuoteRoundedIcon from '@mui/icons-material/RequestQuoteRounded';
-import LogoutRoundedIcon from '@mui/icons-material/LogoutRounded';
+import SportsBaseballIcon from "@mui/icons-material/SportsBaseball";
+import RequestQuoteRoundedIcon from "@mui/icons-material/RequestQuoteRounded";
+import LogoutRoundedIcon from "@mui/icons-material/LogoutRounded";
 
+const Sidebar = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(false);
 
+  const menuItem = [
+    {
+      title: "Dashboard",
+      icon: <DashboardOutlinedIcon />,
+      link: "/",
+    },
+    {
+      title: "Teams",
+      icon: <Groups3RoundedIcon />,
+      link: "/teams",
+    },
+    {
+      title: "Sports",
+      icon: <SportsBaseballIcon />,
+      link: "/sports",
+    },
+    {
+      title: "Tournament",
+      icon: <GiTrophyCup />,
+      link: "/tournament",
+    },
+    {
+      title: "Winner",
+      icon: <GiPodium />,
+      link: "/winners",
+    },
+    {
+      title: "Fee Records",
+      icon: <RequestQuoteRoundedIcon />,
+      link: "/feeRecords",
+    },
+    {
+      title: "Log out",
+      icon: <LogoutRoundedIcon />,
+      link: "/dashboard",
+    },
+  ];
 
-const Sidebar = ({children}) => {
-    const[isOpen ,setIsOpen] = useState(false);
-    const menuItem=[
-        {
-            title:"Dashboard",
-            icon: <DashboardOutlinedIcon/>,
-            link: "/"
-        },
-        {
-            title:"Teams",
-            icon: <Groups3RoundedIcon/>,
-            link: "/teams"
-        },
-        {
-            title:"Sports",
-            icon: <SportsBaseballIcon/>,
-            link: "/sports"
-        },
-        {
-            title:"Tournament",
-            icon: <GiTrophyCup/>,
-            link: "/tournament"
-        },
-        {
-            title:"Winner",
-            icon: <GiPodium/>,
-            link: "/winners"
-        },
-        {
-            title:"Fee Records",
-            icon: <RequestQuoteRoundedIcon/>,
-            link: "/feeRecords"
-        },
-        {
-            title:"Log out",
-            icon: <LogoutRoundedIcon/>,
-            link: "/dashboard"
-        },
-    ]
-    return (
-        <div className="container">
-           <div className="sidebar">
-               <div className="top_section">
-                   <h1 className="logo">Logo</h1>
-               </div>
-               {
-                   menuItem.map((item, index)=>(
-                       <NavLink to={item.link} key={index} className="link" activeclassName="active">
-                           <div id="icon" >{item.icon}</div>
-                           <div id="title" >{item.title}</div>
-                       </NavLink>
-                   ))
-               }
-           </div>
-           <main>{children}</main>
+  return (
+    <>
+      <div className="container">
+        <div className="sidebar">
+          <div className="top_section">
+            <h1 className="logo">Logo</h1>
+          </div>
+          {menuItem.map((item, index) => (
+            <NavLink
+              to={`/admin${item.link}`}
+              key={index}
+              className="link"
+              activeclassname="active"
+            >
+              <div id="icon">{item.icon}</div>
+              <div id="title">{item.title}</div>
+            </NavLink>
+          ))}
         </div>
-    );
+
+        {/* Rendered thru children props(First Version) */}
+        {/* <main>{children}</main> */}
+
+        <Outlet />
+      </div>
+    </>
+  );
 };
 
 export default Sidebar;
+
+

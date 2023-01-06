cefc34ef489fdd37808b0eddc7bce99de9b04c58
CrazyCoder122 <prodoshcmitterfeluda@gmail.com>
2023-01-06 22:16:42 +0530

    Resolved context issue: (Never ever use small letters as first letter in React Component!!)

--- src/components/SportsNavbar.jsx
+++ src/components/SportsNavbar.jsx
@@ -1,78 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { NavLink, Outlet, useOutletContext } from "react-router-dom";
 import "./Sportsbar.css";
 
-import Chess from "../assets/icons/Chess1.png";
-import Races from "../assets/icons/Race.png";
-import Cricket from "../assets/icons/Cricket1.png";
-import Football from "../assets/icons/Football1.png";
-import Volleyball from "../assets/icons/Volleyball1.png";
-import Badminton from "../assets/icons/Badminton.png";
-import Carrom from "../assets/icons/carrom2.png";
+import {menuItem} from '../data/sportsList';
+
+import { SelectedSports } from "../context/context";
 const SportsNavbar = () => {
-
-  const menuItem = [
-    {
-      title: "Chess",
-      icon: <img src={Chess} height='55px' />,
-      
-    },
-    {
-      title: "Races",
-      icon: <img src={Races} height='50px' />,
-      
-    },
-    {
-      title: "Cricket",
-      icon: <img src={Cricket} height='55px' />,
-      
-    },
-    {
-      title: "Football",
-      icon: <img src={Football} height='50px' />,
-      
-    },
-    {
-      title: "Volleyball",
-      icon: <img src={Volleyball} height='50px'/>,
-      
-    },
-    {
-      title: "Badminton",
-      icon: <img src={Badminton} height='50px' />,
-      
-    },
-    {
-      title: "Carrom",
-      icon: <img src={Carrom} height='55px'/>,
-      
-    },
-  ];
-  
-    const [Selectedsports, setSelectedsports] = useState(menuItem[Math.floor(menuItem.length /2)].title)
-
-
+  const ctx = useContext(SelectedSports);
 
   return (
     <>
       <div className="sports-container">
         <div className="sportsbar">
           {menuItem.map((item, index) => (
-            <li to={item.link}
-              key={index} className="sports_id" value={item.title}  onClick={() => setSelectedsports(item.title)}>
-              <div className="holder"><div id="sports_icon">{item.icon}</div></div>
+            <li
+              to={item.link}
+              key={index}
+              className="sports_id"
+              value={item.title}
+              onClick={() => ctx.toggleActiveSport(item.title)}
+            >
+              <div className="holder">
+                <div id="sports_icon">{item.icon}</div>
+              </div>
               <div id="sports_title">{item.title}</div>
-              <div className={Selectedsports === item.title ? "selected" : ""}></div>
+              <div
+                className={ctx.activeSport === item.title ? "selected" : ""}
+              ></div>
             </li>
           ))}
         </div>
 
         {/* Rendered thru children props(First Version) */}
         {/* <main>{children}</main> */}
-
       </div>
     </>
   );
 };
 
-export default SportsNavbar
+export default SportsNavbar;
--- src/pages/Teams.jsx
+++ src/pages/Teams.jsx
@@ -1,27 +1,27 @@
 import React,{useState} from 'react'
 import Sportsbar from "../components/SportsNavbar.jsx";
 
-import { selectedSports } from '../context/context.js';
+import { SelectedSports } from '../context/context.js';
 
 // Later we will pull it from DB
 import {menuItem} from '../data/sportsList'
 
 
 function Teams() {
-  // const [activeSport,setActiveSport] = useState(menuItem[Math.floor(menuItem.length / 2)]);
+  const [activeSport,setActiveSport] = useState(menuItem[Math.floor(menuItem.length / 2)].title);
 
-  // const sportsContextObj = {
-  //   activeSport : activeSport,
-  //   toggleActiveSport : setActiveSport
-  // };
+  const sportsContextObj = {
+    activeSport : activeSport,
+    toggleActiveSport : setActiveSport
+  };
 
   return (
 
-    // <selectedSports.Provider value={sportsContextObj}>
+    <SelectedSports.Provider value={sportsContextObj}>
     <div>
       <Sportsbar />
     </div>
-    // </selectedSports.Provider>
+   </SelectedSports.Provider>
   )
 }
 

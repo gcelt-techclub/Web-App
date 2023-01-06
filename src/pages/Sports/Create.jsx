149e0f42bb781282114c997b331ec4576ed1db83
PRIME_SAPTARSHI <chatterjeeu7@gmail.com>
2023-01-06 11:47:38 +0530

    create page implementation

--- src/pages/Sports/Create.jsx
+++ src/pages/Sports/Create.jsx
@@ -1,72 +1,64 @@
 import React, { useState, useEffect } from 'react'
-import { Stack , Grid, styled, TextField, Paper, makeStyles } from '@mui/material'
+import { Stack, InputAdornment, styled, TextField, Grid, Button } from '@mui/material'
+import { purple } from '@mui/material/colors';
+
+const CSSTextField = styled((props) => (
+  <TextField InputProps={{ disableUnderline: true, style: { fontSize: 20, backgroundColor: '#2e325c', border: '2px solid #3b4285', borderRadius: '10px', color: '#fff', } }}
+    InputLabelProps={{ style: { fontSize: 20, color: '#fff' } }} {...props} />
+))(() => ({}));
+
+const RedTextField = styled((props) => (
+  <TextField InputProps={{ disableUnderline: true, style: { fontSize: 20, backgroundColor: '#F75555', border: '2px solid #F75555', borderRadius: '30px', color: '#fff', padding: 1, } }}
+    InputLabelProps={{ style: { fontSize: 20, color: '#fff' } }} {...props} />
+))(() => ({}));
 
 
-const Item = styled(Paper)(({ theme }) => ({
+const ColorButton = styled(Button)(({ theme }) => ({
+  color: theme.palette.getContrastText(purple[500]),
   backgroundColor: '#2e325c',
-  padding: theme.spacing(1),
-  textAlign: 'center',
-  border: '2px solid #3b4285', borderRadius: '10px',
-  color: '#fff',
+  border: '2px solid #3b4285',
+  borderRadius: '20px',
+  fontSize: 20,
+  '&:hover': {
+    backgroundColor: '#5442CC',
+  },
+  '&:active': {
+    backgroundColor: '#5442CC',
+  },
 }));
 
 function Create() {
   return (
     <form>
-      <Grid container rowSpacing={2} columnSpacing={3}>
-        <Grid item xs={5}>
-          <Item clasName="field" style={{ width: 420, height: 40 }} elevation={8}>
-            <TextField id="standard" style={{ width: 100, maxheight: 10 }} label="standard" variant="standard" />
-          </Item>
-        </Grid>
-        <Grid item xs={2.5}>
-          <Item clasName="field" style={{ width: 190, height: 40 }} elevation={8}>
-            <Stack direction="row" spacing={2}>
-              <div>Team Size</div>
-              <TextField id="standard" style={{ width: 50, maxheight: 10 }} label="standard" variant="standard"
-              inputProps={{style: {fontSize: 25, top:-40}}}  />
-            </Stack>
-          </Item>
-        </Grid>
-        <Grid item xs={2.5}>
-          <Item clasName="field" style={{ width: 190, height: 40 }} elevation={8}>
-            <TextField id="standard" style={{ width: 100, maxheight: 10 }} label="standard" variant="standard" />
-          </Item>
-        </Grid>
-        <Grid item xs={1.5}>
-          <Item style={{ height: 40, borderRadius: '30px', border: '2px solid #F75555', backgroundColor: '#F75555' }}
-            elevation={8}>
-            <TextField style={{ width: 100, maxheight: 10 }} id="standard-number" label="Number" type="number"
-              InputLabelProps={{ shrink: true, }} variant="standard" />
-          </Item>
-        </Grid>
+      <Stack direction="row" Spacing={5}>
+        <CSSTextField id="sportsName" style={{ width: 500, maxheight: 40 }} label="sports name" variant="outlined" />
+        <CSSTextField id="teamSize" style={{ width: 170, maxheight: 40 }} label="Team Size" variant="outlined" />
+        <CSSTextField id="Substitute" style={{ width: 170, maxheight: 40 }} label="Substitute" variant="outlined" />
+        <RedTextField id="entryFee" style={{ width: 120, height: 40 }}
+          label="Entry Fee" variant="outlined" />
+      </Stack>
+      <Grid container rowSpacing={3} columnSpacing={3}>
         <Grid item xs={7.5}>
           <h3>Rules and Guidelines</h3>
         </Grid>
         <Grid item xs={4}>
           <h3>Field View</h3>
         </Grid>
-        <Grid item xs={7.5}>
-          <Item style={{ width: 650, height: '20rem' }} elevation={8}>
-            <TextField id="standard" style={{ width: '100%' }}
-              multiline
-              rows={13}
-              maxRows={13} variant="standard" />
-          </Item>
-        </Grid>
-        <Grid item xs={4}>
-          <Item style={{ width: 200, height: '20rem' }} elevation={8}>xs=8</Item>
-        </Grid>
-        <Grid item xs={7.5}>
-          <></>
-        </Grid>
-        <Grid item xs={2}>
-          <Item style={{ width: 150, height: 30, backgroundColor: '#5442CC' }} elevation={8}>Save</Item>
-        </Grid>
-        <Grid item xs={2}>
-          <Item style={{ width: 150, height: 30 }} elevation={8}>Cancel</Item>
-        </Grid>
       </Grid>
+      <Stack>
+        <Stack>
+          <CSSTextField id="Rules" style={{ width: 650, height: 200 }}
+            multiline
+            rows={11}
+            maxRows={11}
+            label="Rules"
+            variant="outlined" />
+        </Stack>
+        <Stack direction="row" Spacing="5px">
+          <ColorButton variant="contained" size="large" >Save</ColorButton>
+          <ColorButton variant="contained" size="large">Cancel</ColorButton>
+        </Stack>
+      </Stack>
     </form>
   )
 }
